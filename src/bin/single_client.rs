1374977b7e9ecb07a8770d44405523e615772869
GraDje13 <22505651+GraDje13@users.noreply.github.com>
2022-01-24 23:44:30 +0100

    implemented anyhow

--- src/bin/single_client.rs
+++ src/bin/single_client.rs
@@ -1,7 +1,9 @@
+// TODO fix bug with reconnecting
+
+use anyhow::{anyhow, Context};
 use std::env;
 use std::io::{stdin, Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -10,7 +12,7 @@ const SOH: u8 = 1;
 const EOT: u8 = 4;
 const EXIT_COMMAND: &str = "!exit";
 
-fn main() -> Result<(), &'static str> {
+fn main() -> anyhow::Result<()> {
     // griefing silly windows users
     if cfg!(windows) {
         for _i in 1..100 {
@@ -19,41 +21,34 @@ fn main() -> Result<(), &'static str> {
     }
 
     // collect the command line arguments
-    let mut args: Vec<String> = Vec::with_capacity(ARG_NUMBER);
-    args = env::args().collect();
+    let mut args: Vec<String> = env::args().collect();
 
     if args.len() != ARG_NUMBER {
-        return Err("wrong number of arguments");
+        return Err(anyhow!("failed to create listener"));
     }
 
     let socket_address = args.remove(2);
     let remote_adress = args.remove(1);
 
     // make the listener
-    let listener = match TcpListener::bind(socket_address) {
-        Ok(list) => list,
-        Err(e) => {
-            println!("{:?}", e);
-            return Err("Failed to create listener");
-        }
-    };
+    let listener = TcpListener::bind(socket_address).context("failed to make listener")?;
 
-    let listener_thread = thread::spawn(move || {
+    let _listener_thread = thread::spawn(move || -> anyhow::Result<()> {
         loop {
             println!("awaiting remote connection...");
-            let (mut socket, address) = match listener.accept() {
+            let (mut socket, _address) = match listener.accept() {
                 Ok((sock, addr)) => {
                     println!("Connection with: {:?}", addr);
                     (sock, addr)
                 }
-                Err(_e) => panic!("Connection failed"),
+                Err(_e) => return Err(anyhow!("Accepting the connection failed")),
             };
 
             loop {
                 // wait till a message comes
                 let mut start_buffer = [0];
 
-                socket.read_exact(&mut start_buffer);
+                socket.read_exact(&mut start_buffer)?;
 
                 // see if other user has send disconnect message
                 if start_buffer[0] == EOT {
@@ -63,7 +58,8 @@ fn main() -> Result<(), &'static str> {
 
                 // if a message is detected, read it and display it
                 if start_buffer[0] == SOH {
-                    println!("{}", message_read(&mut socket));
+                    let message = message_read(&mut socket)?;
+                    println!("{}", message);
                 }
 
                 //so it does not suck to much cpu
@@ -77,35 +73,36 @@ fn main() -> Result<(), &'static str> {
     loop {
         let mut message_buffer = String::from("");
 
-        stdin().read_line(&mut message_buffer).unwrap();
+        stdin().read_line(&mut message_buffer)?;
         message_buffer.pop();
 
         if message_buffer == EXIT_COMMAND {
-            disconnect(&mut remote_socket);
+            disconnect(&mut remote_socket)?;
             break;
         } else {
-            message_send(&message_buffer, &mut remote_socket);
+            message_send(&message_buffer, &mut remote_socket)?;
         }
     }
 
     Ok(())
 }
 
-fn message_send(message: &str, socket: &mut TcpStream) {
+fn message_send(message: &str, socket: &mut TcpStream) -> anyhow::Result<()> {
     let message_length = message.len();
-    socket.write_all(&[SOH]).unwrap();
-    socket.write_all(&message_length.to_le_bytes()).unwrap();
-    socket.write_all(message.as_bytes()).unwrap();
+    socket.write_all(&[SOH])?;
+    socket.write_all(&message_length.to_le_bytes())?;
+    socket.write_all(message.as_bytes())?;
+    Ok(())
 }
 
-fn message_read(socket: &mut TcpStream) -> String {
+fn message_read(socket: &mut TcpStream) -> anyhow::Result<String> {
     let mut length_buffer = [0; 8];
-    socket.read_exact(&mut length_buffer).unwrap();
+    socket.read_exact(&mut length_buffer)?;
 
     let mut message_buffer = vec![0; usize::from_le_bytes(length_buffer)];
-    socket.read_exact(&mut message_buffer).unwrap();
+    socket.read_exact(&mut message_buffer)?;
 
-    String::from_utf8(message_buffer).unwrap()
+    Ok(String::from_utf8(message_buffer.to_vec())?)
 }
 
 fn connect_until_success(addr: &str) -> TcpStream {
@@ -119,7 +116,8 @@ fn connect_until_success(addr: &str) -> TcpStream {
     }
 }
 
-fn disconnect(socket: &mut TcpStream) {
-    socket.write_all(&[EOT]).unwrap();
-    socket.shutdown(Shutdown::Both);
+fn disconnect(socket: &mut TcpStream) -> anyhow::Result<()> {
+    socket.write_all(&[EOT])?;
+    socket.shutdown(Shutdown::Both)?;
+    Ok(())
 }
